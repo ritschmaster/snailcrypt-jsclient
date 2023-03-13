41ad55604d550e68da399caca408695aae565ef2
Richard Bäck <richard.baeck@mailbox.org>
2023-03-13 18:10:12 +0100

    Adds an appearance switcher.

--- src/webapp/controller/Footer.controller.js
+++ src/webapp/controller/Footer.controller.js
@@ -11,13 +11,35 @@ sap.ui.define([
     popupFacade: null,
 
     onInit: function () {
+	  var me = this;
+		
       var i18nModel = new ResourceModel({
         bundleName: "snailcrypt-jsclient.i18n.i18n"
       });
       this.getView().setModel(i18nModel, "i18n");
 
       this.popupFacade = new PopupFacade(this.getView().getModel("i18n").getResourceBundle());
+      
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    	this.setDarkMode();	
+	  }
+	  
+	  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+		if (event.matches) {
+		  	this.setDarkMode();		
+		} else {
+			this.setLightMode();
+		}
+	  });
     },
+    
+    setLightMode: function() {
+		sap.ui.getCore().applyTheme("sap_belize");	
+	},
+	
+	setDarkMode: function() {
+		sap.ui.getCore().applyTheme("sap_fiori_3_dark");		
+	},
 
     onFooterImpressumtPressed: function () {
       this.popupFacade.showImpressumPopup();
@@ -33,6 +55,14 @@ sap.ui.define([
 
     onFlagDEPressed: function() {
       sap.ui.getCore().getConfiguration().setLanguage("de");
-    }
+    },
+    
+    onLightModePressed: function() {
+		this.setLightMode();	
+	},
+	
+	onDarkModePressed: function() {
+		this.setDarkMode();
+	}
   });
 });
